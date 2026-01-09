d49b75354fc2df174e9d2da22c868bb43a692c81
chandrakhushi <khushi.chandra@outlook.com>
2026-01-08 17:40:31 -0800

    Initial Commit

---
+++ goals/goalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  goalsApp.swift
+//  goals
+//
+//  Created by Khushi Chandra on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct goalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
